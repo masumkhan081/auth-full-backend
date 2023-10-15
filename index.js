f515f72c4fbb0bb37714490260ca244f95522430
masum khan <study.contents.of.masumkhan@gmail.com>
2023-10-16 04:18:57 +0600

    pokath

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req,res,next)=>{
   res.header({ "Access-Control-Allow-Origin": "http://localhost:5173" })
   next();
 })
-// app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5173" }));
 //
 
 // const whitelist = ['http://localhost:5173', 'http://blablabla.com'];
