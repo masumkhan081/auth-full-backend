5f0016cfb52063cd86006ad287a5784c085ea12c
masum khan <study.contents.of.masumkhan@gmail.com>
2023-10-16 15:36:41 +0600

    pokath

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ initDB();
 // middlewares
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(originControl);
 
