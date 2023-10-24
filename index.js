ad4dc3c55e65ee248c049bb72246d25b33440fb0
masum khan <study.contents.of.masumkhan@gmail.com>
2023-10-24 23:22:20 +0600

    pokath

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ initDB();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-// http://localhost:5173
-app.use(cors({ origin: "https://auth-full.vercel.app", credentials: true }));
+// https://auth-full.vercel.app
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(originControl);
 
 // routes: login, register, recover, reset, verify, logout ...
--- src/middleware/middlewares.js
+++ src/middleware/middlewares.js
@@ -6,7 +6,7 @@ const userModel = require("../model/user");
 
 function originControl(req, res, next) {
   //   http://localhost:5173
-  res.header({ "Access-Control-Allow-Origin": "https://auth-full.vercel.app" });
+  res.header({ "Access-Control-Allow-Origin": "http://localhost:5173" });
   next();
 }
 
